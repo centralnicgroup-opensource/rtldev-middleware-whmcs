488a3b4955ea67cbf082bb38b5f7cb8b0e2c8eac
Kai Schwarz <kschwarz@hexonet.net>
2018-10-19 15:17:58 +0200

    feat(releaseInfo): add json file covering repository info

--- updateVersion.sh
+++ updateVersion.sh
@@ -7,7 +7,10 @@
 # version format: X.Y.Z
 newversion="$1";
 branch="$2";
+date="$(date +'%Y-%m-%d')";
 
 if [ "$branch" = "master" ]; then
     sed -i "s/\$module_version = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\$module_version = \"${newversion}\"/g" registrars/ispapi/ispapi.php
+    sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${newversion}\"/g" release.json
+    sed -i "s/\"date\": \"[0-9]\+-[0-9]\+-[0-9]\+\"/\"date\": \"${date}\"/g" release.json
 fi;
