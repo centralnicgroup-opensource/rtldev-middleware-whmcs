34d2af1d7287dcbe40bbd29173f5df3308c6cd68
Kai Schwarz <kschwarz@hexonet.net>
2018-11-30 17:40:08 +0100

    chore(release): review rebase script #2

--- rebaseDevBranch.sh
+++ rebaseDevBranch.sh
@@ -6,6 +6,7 @@ git fetch origin -p
 git checkout master -f &&
 git pull &&
 git checkout "$DEVBRANCH" -f &&
+git pull &&
 git rebase master &&
 git push --force-with-lease
 git checkout "$OLDBRANCH"
