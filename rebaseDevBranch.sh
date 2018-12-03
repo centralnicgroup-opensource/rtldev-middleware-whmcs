80ae68dd2163fdb5416ed90a74c9e091b16764ea
Kai Schwarz <kschwarz@hexonet.net>
2018-12-03 09:55:26 +0100

    refactor(release): review rebase script

--- rebaseDevBranch.sh
+++ rebaseDevBranch.sh
@@ -2,12 +2,18 @@
 OLDBRANCH=$(git branch | sed -n '/\* /s///p')
 DEVBRANCH="$1";
 
-git fetch origin -p
+git fetch origin -p &&
 git checkout master -f &&
 git pull &&
 git checkout "$DEVBRANCH" -f &&
 git pull &&
 git rebase master &&
-git push --force-with-lease
+git push --force-with-lease &&
 git checkout "$OLDBRANCH"
-exit "$?";
+# shellcheck disable=SC2181
+if [ "$?" -eq 0 ]; then
+    echo "Rebase of branch next: SUCCEEDED"
+else
+    echo "Rebase of branch next: FAILED"
+fi
+exit 0
