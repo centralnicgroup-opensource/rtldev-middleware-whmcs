7ce97735333f3779a4a5da6b1579dc4239541360
Kai Schwarz <kschwarz@hexonet.net>
2020-05-28 16:44:08 +0200

    fix(ci): fixed version update process
    fix #148

--- registrars/ispapi/ispapi.php
+++ registrars/ispapi/ispapi.php
@@ -5,7 +5,7 @@
  * @author HEXONET GmbH <support@hexonet.net>
  */
 
-define("ISPAPI_MODULE_VERSION", "3.0.5");
+define("ISPAPI_MODULE_VERSION", "3.0.7");
 
 if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
--- updateVersion.sh
+++ updateVersion.sh
@@ -8,6 +8,6 @@
 newversion="$1";
 date="$(date +'%Y-%m-%d')";
 
-sed -i "s/define('ISPAPI_MODULE_VERSION', '[0-9]\+\.[0-9]\+\.[0-9]\+')/define('ISPAPI_MODULE_VERSION', '${newversion}')/g" registrars/ispapi/ispapi.php
+sed -i "s/define(\"ISPAPI_MODULE_VERSION\", \"[0-9]\+\.[0-9]\+\.[0-9]\+\")/define(\"ISPAPI_MODULE_VERSION\", \"${newversion}\")/g" registrars/ispapi/ispapi.php
 sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${newversion}\"/g" release.json
 sed -i "s/\"date\": \"[0-9]\+-[0-9]\+-[0-9]\+\"/\"date\": \"${date}\"/g" release.json
