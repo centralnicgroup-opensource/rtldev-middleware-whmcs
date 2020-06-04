cb2672cb49bd1f7010d23b588c96011e25d94fa2
Kai Schwarz <kschwarz@hexonet.net>
2020-06-04 14:12:16 +0200

    fix(irtp): review configuration to by default suggest the WHMCS way for IRTP

--- updateVersion.sh
+++ updateVersion.sh
@@ -9,5 +9,6 @@ newversion="$1";
 date="$(date +'%Y-%m-%d')";
 
 sed -i "s/define(\"ISPAPI_MODULE_VERSION\", \"[0-9]\+\.[0-9]\+\.[0-9]\+\")/define(\"ISPAPI_MODULE_VERSION\", \"${newversion}\")/g" registrars/ispapi/ispapi.php
+sed -i "s/\"ISPAPI v[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"ISPAPI v${newversion}\"/g" registrars/ispapi/whmcs.json
 sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${newversion}\"/g" release.json
 sed -i "s/\"date\": \"[0-9]\+-[0-9]\+-[0-9]\+\"/\"date\": \"${date}\"/g" release.json
