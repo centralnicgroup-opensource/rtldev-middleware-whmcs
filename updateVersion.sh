da8a3aa11070d299e3b37b03bc422d958a02678b
Kai Schwarz <kschwarz@hexonet.net>
2020-07-16 14:51:07 +0200

    build(versionupdater): safe sed usage in script

--- updateVersion.sh
+++ updateVersion.sh
@@ -5,10 +5,17 @@
 # semantic-release. SEE package.json
 
 # version format: X.Y.Z
-newversion="$1";
-date="$(date +'%Y-%m-%d')";
+newversion="$1"
+date="$(date +'%Y-%m-%d')"
 
-sed -i "s/define(\"ISPAPI_MODULE_VERSION\", \"[0-9]\+\.[0-9]\+\.[0-9]\+\")/define(\"ISPAPI_MODULE_VERSION\", \"${newversion}\")/g" registrars/ispapi/ispapi.php
-sed -i "s/\"ISPAPI v[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"ISPAPI v${newversion}\"/g" registrars/ispapi/whmcs.json
-sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${newversion}\"/g" release.json
-sed -i "s/\"date\": \"[0-9]\+-[0-9]\+-[0-9]\+\"/\"date\": \"${date}\"/g" release.json
+printf -v sed_script 's/define("ISPAPI_MODULE_VERSION", "[0-9]\+\.[0-9]\+\.[0-9]\+")/define("ISPAPI_MODULE_VERSION", "%s")/g' "${newversion}"
+sed -i -e "${sed_script}" registrars/ispapi/ispapi.php
+
+printf -v sed_script 's/"ISPAPI v[0-9]\+\.[0-9]\+\.[0-9]\+"/"ISPAPI v%s"/g' "${newversion}"
+sed -i -e "${sed_script}" registrars/ispapi/whmcs.json
+
+printf -v sed_script 's/"version": "[0-9]\+\.[0-9]\+\.[0-9]\+"/"version": "%s"/g' "${newversion}"
+sed -i -e "${sed_script}" release.json
+
+printf -v sed_script 's/"date": "[0-9]\+-[0-9]\+-[0-9]\+"/"date": "%s"/g' "${date}"
+sed -i -e "${sed_script}" release.json
