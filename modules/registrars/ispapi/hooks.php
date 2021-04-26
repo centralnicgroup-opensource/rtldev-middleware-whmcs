c0abb909a4a7eb1e791ce86ed28ec6fa53c0f31f
Kai Schwarz <kschwarz@hexonet.net>
2021-04-26 12:03:02 +0200

    refactor(cleanup): added some cleanup & code review

--- modules/registrars/ispapi/hooks.php
+++ modules/registrars/ispapi/hooks.php
@@ -2,23 +2,24 @@
 
 use WHMCS\Module\Registrar\Ispapi\Ispapi;
 use WHMCS\Module\Registrar\Ispapi\Helper;
+use WHMCS\Module\Registrar\Ispapi\Domain as HXDomain;
 
 $path = implode(DIRECTORY_SEPARATOR, [__DIR__, "hooks_migration.php"]);
 if (file_exists($path)) {
     include $path;
 }
 
-add_hook('ClientAreaHeadOutput', 1, function ($vars) {
+add_hook("ClientAreaHeadOutput", 1, function ($vars) {
     // Auto-Prefill VAT-ID, X-DK-REGISTRANT/ADMIN additional domain field when provided in client data
-    $ispapilang = $vars['clientsdetails']['language'];
-    $ispapivatid = $vars['clientsdetails']['tax_id'];
-    $ispapidkid = '';
+    $ispapilang = $vars["clientsdetails"]["language"];
+    $ispapivatid = $vars["clientsdetails"]["tax_id"];
+    $ispapidkid = "";
 
-    if (function_exists('getCustomFields')) {
-        $cfs = getCustomFields("client", "", $vars['clientsdetails']['userid'], "on", "");
+    if (function_exists("getCustomFields")) {
+        $cfs = getCustomFields("client", "", $vars["clientsdetails"]["userid"], "on", "");
         foreach ($cfs as $cf) {
-            if ("dkhostmasteruserid" === $cf['textid'] && !empty($cf['value'])) {
-                $ispapidkid = strtoupper($cf['value']);
+            if ("dkhostmasteruserid" === $cf["textid"] && !empty($cf["value"])) {
+                $ispapidkid = strtoupper($cf["value"]);
             }
         }
     }
@@ -59,24 +60,24 @@ HTML;
     }
 });
 
-add_hook('AfterRegistrarRegistrationFailed', 1, function ($vars) {
+add_hook("AfterRegistrarRegistrationFailed", 1, function ($vars) {
     // ONLY FOR .SWISS: saves the .swiss application ID in the admin note
     $params = $vars["params"];
     $domain = $params["sld"] . "." . $params["tld"];
-    if (preg_match('/[.]swiss$/i', $domain)) {
-        preg_match('/<#(.+?)#>/i', $vars["error"], $matches);
+    if (preg_match("/[.]swiss$/i", $domain)) {
+        preg_match("/<#(.+?)#>/i", $vars["error"], $matches);
         if (isset($matches[1])) {
             $application_id = $matches[1];
             $data = [
                 ":id" => $params["domainid"],
-                ":notes" => '### DO NOT DELETE ANYTHING BELOW THIS LINE \nAPPLICATION:".$application_id."\n'
+                ":notes" => "### DO NOT DELETE ANYTHING BELOW THIS LINE \nAPPLICATION:" . $application_id . "\n"
             ];
             Helper::SQLCall("UPDATE tbldomains SET additionalnotes=:notes WHERE id=:id", $data, "execute");
         }
     }
 });
 
-add_hook('DailyCronJob', 1, function ($vars) {
+add_hook("DailyCronJob", 1, function ($vars) {
     // Submit Statistics
     $authOk = Ispapi::checkAuth();
     if (!$authOk) {
@@ -90,10 +91,14 @@ add_hook('DailyCronJob', 1, function ($vars) {
         ":registrar" => "ispapi",
         ":stat" => "Pending"
     ];
-    $r = Helper::SQLCall("SELECT id, additionalnotes FROM tbldomains WHERE additionalnotes!='' AND registrar=:registrar AND status=:stat", $data, "fetchall");
+    $r = Helper::SQLCall(
+        "SELECT id, additionalnotes FROM tbldomains WHERE additionalnotes!=\"\" AND registrar=:registrar AND status=:stat",
+        $data,
+        "fetchall"
+    );
     if ($r["success"]) {
         foreach ($r["result"] as $row) {
-            preg_match('/APPLICATION:(.+?)(?:$|\n)/i', $row["additionalnotes"], $matches);
+            preg_match("/APPLICATION:(.+?)(?:$|\n)/i", $row["additionalnotes"], $matches);
             if (isset($matches[1])) {
                 $data = [
                     ":id" => $row["id"]
@@ -117,25 +122,19 @@ add_hook('DailyCronJob', 1, function ($vars) {
     }
 });
 
-add_hook('ClientAreaPageDomainDetails', 1, function ($vars) {
-    // TLDs not supporting Transfer Lock: remove 'Registrar Lock' menu entry.
-    $domain = Menu::context('domain');
+add_hook("ClientAreaPageDomainDetails", 1, function ($vars) {
+    // TLDs not supporting Transfer Lock: remove "Registrar Lock" menu entry.
+    $domain = Menu::context("domain");
 
     if ($domain->registrar == "ispapi") {
-        $r = Ispapi::call([
-            "COMMAND" => "QueryDomainList",
-            "VERSION" => 2,
-            "DOMAIN" => $domain->domain,
-            "WIDE" => 1,
-            "NOTOTAL" => 1
-        ], getregistrarconfigoptions("ispapi"));
+        $r = HXDomain::getRegistrarLock(getregistrarconfigoptions("ispapi"), $domain->domain);
 
-        if (($r['CODE'] == 200) && ($r['PROPERTY']['TRANSFERLOCK'] && $r['PROPERTY']['TRANSFERLOCK'][0] == "")) {
-            $vars['managementoptions']['locking'] = false;
-            $vars['lockstatus'] = false;
-            $menu = $vars['primarySidebar']->getChild('Domain Details Management');
+        if (isset($r["error"])) {
+            $vars["managementoptions"]["locking"] = false;
+            $vars["lockstatus"] = false;
+            $menu = $vars["primarySidebar"]->getChild("Domain Details Management");
             if (!is_null($menu)) {
-                $menu->removeChild('Registrar Lock Status');
+                $menu->removeChild("Registrar Lock Status");
             }
         }
 
