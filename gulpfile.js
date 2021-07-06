1edf62b91f812f16627b7aa1794aca00b5d770ca
Kai Schwarz <kschwarz@hexonet.net>
2021-07-06 09:19:37 +0200

    ci(gulp): minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
+const path = require('path')
 const { series, src, dest } = require('gulp')
 const clean = require('gulp-clean')
 const zip = require('gulp-zip')
-const tar = require('gulp-tar')
-const gzip = require('gulp-gzip')
+const gexec = require('gulp-exec')
+const gap = require('gulp-append-prepend')
+const replace = require('gulp-replace')
+const tap = require('gulp-tap')
+const removeEmptyLines = require('gulp-remove-empty-lines')
+const EOL = require('os').EOL
 const exec = require('util').promisify(require('child_process').exec)
 const cfg = require('./gulpfile.json')
 
@@ -34,8 +39,13 @@ async function doLint()
  */
 function doDistClean()
 {
-    return src([cfg.archiveBuildPath, `${cfg.archiveFileName} - latest.zip`], { read: false, base: '.', allowEmpty: true })
-    .pipe(clean({ force: true }))
+    return src([
+        cfg.archiveBuildPath,
+        cfg.archiveFileName,
+        cfg.archiveFileNameEncrypted
+      ],
+      { read: false, base: '.', allowEmpty: true }
+    ).pipe(clean({ force: true }))
 }
 
 /**
@@ -45,7 +55,7 @@ function doDistClean()
 function doCopyFiles()
 {
     return src(cfg.filesForArchive, { base: '.' })
-    .pipe(dest(cfg.archiveBuildPath))
+      .pipe(dest(cfg.archiveBuildPath))
 }
 
 /**
@@ -55,46 +65,94 @@ function doCopyFiles()
 function doFullClean()
 {
     return src(cfg.filesForCleanup, { read: false, base: '.', allowEmpty: true })
-    .pipe(clean({ force: true }))
+      .pipe(clean({ force: true }))
 }
 
 /**
- * build latest zip archive
+ * Encrypt files using IONCube
+ * @return Promise
+ */
+ function doEncrypt() {
+  //let index = 0;
+  const anchor = '<?php';
+
+  // new Promise(function (resolve, reject) {
+  return src(cfg.filesForEncrypt, { base: '.' })
+      .pipe(replace(anchor, ''))//remove first line (php tag)
+      .pipe(removeEmptyLines())//remove empty lines
+      /*.pipe(tap(function (file, t) {//add ioncube dynamic key fn and key comment
+          index++;
+          const filename = path.basename(file.path)
+          const value = (`migrationTool${filename}`).split("").reverse().join("")
+          const msg = [
+              anchor,
+              `function MIG_KEYGEN${index}($v, $w) {`,
+              '    return strrev($v.$w);',
+              '}',
+              `// @ioncube.dk MIG_KEYGEN${index}("migrationTool", "${filename}") -> "${value}"`
+          ].join(EOL)
+          return t.through(gap.prependText, [msg])
+      }))*/
+      .pipe(dest('tmp'))//save this to a temporary folder
+      //encrypt the temporary build files using ionCube Encoder
+      .pipe(gexec(file => `${cfg.ioncubecmd} ${file.path} -o ${cfg.archiveBuildPath}/${file.path.replace(/^.+\/tmp\//, '')}`, {
+          continueOnError: false, // default = false, true means don't emit error event
+          pipeStdout: false, // default = false, true means stdout is written to file.contents
+      }))
+}
+
+/**
+ * build zip archive with encrypted files in it
  * @return stream
  */
-function doGitZip() {
-  return src(`./${cfg.archiveBuildPath}/**`)
-    .pipe(zip(`${cfg.archiveFileName}-latest.zip`))
-    .pipe(dest('.'))
+ function doZipEncrypt() {
+  return doZip(cfg.archiveFileNameEncrypted)
+}
+
+/**
+* build zip archive with encrypted files in it
+* @return stream
+*/
+function doZipUnencrypt() {
+  return doZip(cfg.archiveFileName)
 }
 
 /**
  * build zip archive
  * @return stream
  */
-function doZip() {
+function doZip(archiveFileName) {
   return src(`./${cfg.archiveBuildPath}/**`)
-    .pipe(zip(`${cfg.archiveFileName}.zip`))
-    .pipe(dest('./pkg'))
+    .pipe(zip(archiveFileName))
+    .pipe(dest('.'))
 }
 
-/**
- * build tar archive
- * @return stream
- */
-function doTar() {
-  return src(`./${cfg.archiveBuildPath}/**`)
-    .pipe(tar(`${cfg.archiveFileName}.tar`))
-    .pipe(gzip())
-    .pipe(dest('./pkg'))
-}
+exports.archives = series(
+  doZipUnencrypt
+)
+
+exports.archivesEnc = series(
+  doZipEncrypt
+)
+
+exports.fullclean = series(
+  doFullClean
+)
+
+exports.distclean = series(
+  doDistClean
+)
 
 exports.lint = series(
   doLint
 )
 
+exports.encrypt = series(
+  doEncrypt
+)
+
 exports.copy = series(
-  doDistClean,
+  exports.distclean,
   doCopyFiles
 )
 
@@ -103,19 +161,25 @@ exports.prepare = series(
   exports.copy
 )
 
-exports.archives = series(
-  doGitZip,
-  doZip,
-  doTar
+exports.buildEncrypted = series(
+  exports.prepare,
+  exports.encrypt,
+  exports.archivesEnc,
+  exports.fullclean
+)
+
+exports.buildUnencrypted = series(
+  exports.prepare,
+  exports.archives,
+  exports.fullclean
 )
 
 exports.default = series(
   exports.prepare,
   exports.archives,
-  doFullClean
+  exports.fullclean
 )
 exports.release = series(
-  exports.copy,
-  exports.archives,
-  doFullClean
+  exports.buildEncrypted,
+  exports.fullclean
 )
